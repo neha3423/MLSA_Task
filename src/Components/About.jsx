885258d020721c31354b5f615c1a5bbaf4c92b71
neha3423 <akellaneha@gmail.com>
2023-12-26 23:02:58 +0530

    add all commits

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,17 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a CSE undergrad studying at Pragati Engineering College. I enjoy creating unique and simplistic user apps in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
+    "User experience",
+  "Machine Learning",
+  "Web development",
+  "Mobile app development",
+  "database management",
   "Mobile user interfaces",
   "Graphic design",
 ];
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my machine learning experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
